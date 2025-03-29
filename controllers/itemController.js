109576d8a528175e0938a6b2b95f8d1ad62a8664
Faris Sharara <faresshohdy0@gmail.com>
2025-03-29 22:05:04 +0200

    Add delete item functionality with corresponding route and UI integration

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -19,3 +19,12 @@ exports.addItem = async (req, res) => {
     res.status(500).send('Error saving item');
   }
 };
+
+exports.deleteItem = async (req, res) => {
+  try {
+    await Item.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+  } catch (err) {
+    res.status(500).send('Error deleting item');
+  }
+};
